714e47da438e815976d0b810184bf555607205a7
eminenceinthelight <daviizgbemi@gmail.com>
2023-08-31 14:43:29 +0100

    finally got update_expression function to work kind of. It can't solve minus problems though

--- ExS.hpp
+++ ExS.hpp
@@ -23,10 +23,10 @@ namespace ExS
     bool string_a_number(const std::string &expr);
     bool is_an_operator(const char &character);
     bool is_a_minus_or_dot(const char &character);
-    void loop(std::string &expr);
+    std::string loop(std::string expr);
+    std::string update_expression(std::string &expr, double &result, size_t operator_index, IntCharMap &operator_pos_map);
     size_t choose_operator(const ExS::IntCharMap &operator_map);
     std::tuple<double, double> bi_operator_operands(std::string &expr, size_t &priority_index, IntCharMap &operator_pos_map);
-    std::string update_expression(std::string &expr, double &result, size_t operator_index, IntCharMap &operator_pos_map);
     IntCharMap verify_and_collect_pos(const std::string &expr);
 }
 #endif
